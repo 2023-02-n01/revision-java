6355c209dc29f8779d81ae93fd7ab54ec5d946bf
Lionnelle ZONGO MBOM <lionnelle14@outlook.fr>
2023-02-10 15:51:04 +0100

    Random #3

---
+++ jeux-de-Role/src/main/java/entite/ExempleRandom.java
@@ -0,0 +1,19 @@
+package entite;
+
+import java.util.Random;
+
+public class ExempleRandom {
+
+    public static void main(String[] args) {
+        // génération d'un nombre entre 12 et 18
+        Random random = new Random();
+
+        int force = random.nextInt(12, 18);
+        System.out.println(force);
+
+
+        int pointsVie = random.nextInt(20,50);
+        System.out.println(pointsVie);
+
+    }
+}
